0bd730320e7a4e4a6e1620ce12db3bd8ba3c3115
Isabella Karibian <isabellakaribian@gmail.com>
2023-09-14 14:35:49 +0400

    m4Cont4

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryAssignment {
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
+        int num = 10;
+        int num1 = -10;
+        int result = first > second ? num : num1;
+        System.out.println(result);
     }
 }
